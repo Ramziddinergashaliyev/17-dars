3ba9c7f36a997108fe53336ebc6c7c19a6e82b30
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-01-26 00:32:37 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let modeBtn = document.getElementById("mode-btn");
+let navbar = document.getElementById("navbar");
+let backtop = document.getElementById("backtop");
+let navList = document.getElementById("nav__list");
+let barBtn = document.getElementById("nav__hamburger");
+
+barBtn.addEventListener("click", function () {
+  navList.classList.toggle("show-nav__list");
+});
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+function shrink() {
+  if (scrollY > 1) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
